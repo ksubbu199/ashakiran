01feee1c794f46cbbbe42e61d9d444a677f73c8b
Subrahamanyam Varma <ksubbu199@gmail.com>
2017-08-02 01:05:05 +0530

    Added test route to test nodemailer and it worked

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+
+var nodemailer=require('nodemailer');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
@@ -16,5 +18,28 @@ router.get('/schedule', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+router.get('/test',function(req,res,next){
+  let poolConfig = require('../smtp.js').poolConfig;
+  var smtpTransport = nodemailer.createTransport(poolConfig, {});
+  //console.log('hello');
+  //console.log(smtpTransport);
+  var mailOptions = {
+    from: 'ashakiran@students.iiit.ac.in',
+    to: 'ksubbu199@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  smtpTransport.sendMail(mailOptions, function(error, info){
+    res.send(error);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+  });
+res.end();
 });
 module.exports = router;
